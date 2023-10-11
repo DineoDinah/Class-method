1e85b1ba4a3f83b15f06dd9969785f6df430b95f
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-11 09:34:42 +0200

    Add project files.

---
+++ Class method/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(50, "Dineo", 17);
+            Student student2 = new Student(67, "Dinah", 10);
+
+            Console.WriteLine(student1.IsObesse());
+            Console.WriteLine(student2.IsObesse());
+
+            Console.Read();
+        }
+    }
+}
